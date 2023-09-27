37d94069d3b51ad1648a18344fe6d38a4839c72d
goncalhurtado <gonzalomilano@gmail.com>
2023-09-27 16:39:47 -0300

    added library

---
+++ src/components/GoncalCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GoncalCard = () => {
+  return <div>GoncalCard</div>;
+};
+
+export default GoncalCard;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <div>
+        <div className="d-flex justify-content-end">
+          <div className="d-flex">
+            <div>
+              <div>Home</div>
+            </div>
+            <div>
+              <div>Projects</div>
+            </div>
+            <div>
+              <div>Contact</div>
+            </div>
+            <div>
+              <div>Ligth</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
