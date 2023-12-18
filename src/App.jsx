5f12d50999279e43ca3a73ddc41bfcdb0fc95e40
goncalhurtado <gonzalomilano@gmail.com>
2023-12-18 18:27:11 -0300

    added style on navbar comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import GoncalCard from "./components/GoncalCard";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Education from "./pages/Education";
@@ -10,17 +10,21 @@ import Education from "./pages/Education";
 function App() {
   return (
     <div className="container pb-5">
-      <div className="row mb-4">
-        <div className="col-lg-7 col-sm-12"></div>
-        <div className="col-lg-5 col-sm-12">
-          <NavBar />
+      {/* <div className="row mb-4">
+        <div className="col-lg-4 col-sm-12"></div>
+        <div className="col-lg-8 col-sm-12">
+
         </div>
-      </div>
+      </div> */}
       <div className="row">
         <div className="col-lg-4 col-sm-12 justsmmb">
           <GoncalCard />
         </div>
         <div className="col-lg-8 col-sm-12">
+          <div className="mb-3">
+            {" "}
+            <NavBar />
+          </div>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,88 +8,80 @@ const NavBar = () => {
     setMode((prevMode) => (prevMode === "dark" ? "light" : "dark"));
   };
   const handle = (e, link) => {
-    // e.preventDefault();
     setActive(link);
   };
 
   return (
     <>
-      <div className="row">
-        <div className="col-10 navSm">
-          <div className="d-flex justify-content-center navbar">
-            <div className="d-flex align-items-center">
-              <div>
-                <Link to="/" onClick={(e) => handle(e, "home")}>
-                  <div
-                    className={
-                      active === "home"
-                        ? "navbar__link activeLink "
-                        : "navbar__link"
-                    }
-                  >
-                    <i className="bi bi-house-door navbar__link__logo"></i>
-                    <p className="navbar__text m-0">Home</p>
-                  </div>
-                </Link>
-              </div>
-              <div>
-                <Link to="/projects" onClick={(e) => handle(e, "projects")}>
-                  <div
-                    className={
-                      active === "projects"
-                        ? "navbar__link activeLink"
-                        : "navbar__link"
-                    }
-                  >
-                    <i className="bi bi-code-slash navbar__link__logo"></i>
-                    <p className="navbar__text m-0">Projects</p>
-                  </div>
-                </Link>
-              </div>
-              <div className=" navbar__link">
-                <Link to="/education" onClick={(e) => handle(e, "education")}>
-                  <div
-                    className={
-                      active === "education"
-                        ? "navbar__link activeLink"
-                        : "navbar__link"
-                    }
-                  >
-                    <div className="navbar__link__logo">
-                      <i className="bi bi-book"></i>
-                    </div>
-                    <p className="navbar__text m-0">Education</p>
-                  </div>
-                </Link>
-              </div>
-              <div className=" navbar__link">
-                <Link to="/contact" onClick={(e) => handle(e, "contact")}>
-                  <div
-                    className={
-                      active === "contact"
-                        ? "navbar__link activeLink"
-                        : "navbar__link"
-                    }
-                  >
-                    <i className="bi bi-person-lines-fill navbar__link__logo"></i>
-                    <p className="navbar__text m-0">Contact</p>
-                  </div>
-                </Link>
-              </div>
+      <div className="">
+        <div className="navbarContainer">
+          <div className="navbar">
+            <div>
+              <Link to="/" onClick={(e) => handle(e, "home")}>
+                <div
+                  className={
+                    active === "home"
+                      ? "navbar__link activeLink "
+                      : "navbar__link"
+                  }
+                >
+                  <i className="bi bi-house-door "></i>
+                  <p className=" m-0">Home</p>
+                </div>
+              </Link>
             </div>
+            <div>
+              <Link to="/projects" onClick={(e) => handle(e, "projects")}>
+                <div
+                  className={
+                    active === "projects"
+                      ? "navbar__link activeLink"
+                      : "navbar__link"
+                  }
+                >
+                  <i className="bi bi-code-slash"></i>
+                  <p className=" m-0">Projects</p>
+                </div>
+              </Link>
+            </div>
+            <div className=" navbar__link">
+              <Link to="/education" onClick={(e) => handle(e, "education")}>
+                <div
+                  className={
+                    active === "education"
+                      ? "navbar__link activeLink"
+                      : "navbar__link"
+                  }
+                >
+                  <i className="bi bi-book"></i>
+                  <p className=" m-0">Education</p>
+                </div>
+              </Link>
+            </div>
+            {/* <div className=" navbar__link">
+              <Link to="/contact" onClick={(e) => handle(e, "contact")}>
+                <div
+                  className={
+                    active === "contact"
+                      ? "navbar__link activeLink"
+                      : "navbar__link"
+                  }
+                >
+                  <i className="bi bi-person-lines-fill navbar__link__logo"></i>
+                  <p className="navbar__text m-0">Contact</p>
+                </div>
+              </Link>
+            </div> */}
           </div>
-        </div>
-        <div
-          className="col-2 d-flex align-items-center justify-content-center darkMode"
-          onClick={toggleMode}
-        >
-          <i
-            className={
-              mode === "dark"
-                ? "bi bi-moon darkMode__logo"
-                : " bi bi-sun darkMode__logo"
-            }
-          ></i>
+          <div className="darkMode" onClick={toggleMode}>
+            <i
+              className={
+                mode === "dark"
+                  ? "bi bi-moon darkMode__logo"
+                  : " bi bi-sun darkMode__logo"
+              }
+            ></i>
+          </div>
         </div>
       </div>
     </>
