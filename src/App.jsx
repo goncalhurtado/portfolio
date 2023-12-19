2565cc04019e05822a3b9c449552ed55b5f5f9b9
goncalhurtado <gonzalomilano@gmail.com>
2023-12-18 22:00:23 -0300

    changing name to home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import GoncalCard from "./components/GoncalCard";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Education from "./pages/Education";
