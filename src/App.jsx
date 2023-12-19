2025ad62eb589fb9cc716ef42259a4e2fe21de52
goncalhurtado <gonzalomilano@gmail.com>
2023-12-18 21:42:14 -0300

    added style to contacts buttom

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,10 @@ function App() {
         </div>
       </div> */}
       <div className="row">
-        <div className="col-lg-4 col-sm-12 justsmmb">
+        <div className="col-lg-3 col-sm-12 justsmmb">
           <GoncalCard />
         </div>
-        <div className="col-lg-8 col-sm-12">
+        <div className="col-lg-9 col-sm-12">
           <div className="mb-3">
             {" "}
             <NavBar />
--- src/components/GoncalCard.jsx
+++ src/components/GoncalCard.jsx
@@ -3,16 +3,28 @@ import React from "react";
 const GoncalCard = () => {
   return (
     <div className="ContGoncal">
-      <img src="src/assets/img/goncal.jpg" alt="imagen" />
+      <img
+        src="https://res.cloudinary.com/dhvgi2cmq/image/upload/v1702944689/portfolio-Goncal/perfil_slack_kawow7.jpg"
+        alt="imagen"
+      />
       <div className="contInfo">
         <h1 className="contInfo__name">Gonzalo Hurtado</h1>
         <h2 className="contInfo__dev">Front End Developer</h2>
         <hr />
         <h2 className="contInfo__dev">Contact me</h2>
-        <p className="m-0">goncalhurtado@gmail.com</p>
-        <i className="bi bi-github"></i>
-        <i className="bi bi-linkedin"></i>
-        <div className="btn btn-primary">Download CV</div>
+        <p className="m-0 mail">goncalhurtado@gmail.com</p>
+        <div className="btnContact">
+          <i className="bi bi-github"></i>
+          <p className="m-0">Github</p>
+        </div>
+        <div className="btnContact">
+          <i className="bi bi-linkedin"></i>
+          <p className="m-0">Linkedin</p>
+        </div>
+        <div className="btnContact">
+          <i className="bi bi-file-earmark-person"></i>
+          <p className="m-0 ">Resume</p>
+        </div>
       </div>
     </div>
   );
