32be3787e3e2524b2bc098d585b9064c643ee0f2
goncalhurtado <gonzalomilano@gmail.com>
2023-12-21 15:52:33 -0300

    added style on education comp

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,40 +1,14 @@
 import React from "react";
 
-const Education = () => {
+const Education = ({ darkMode }) => {
   return (
-    <div className="card">
-      <div className="row p-4">
-        <div className="col-12 col-sm-6 align-items-center justify-content-center text-start">
-          <div className="d-flex flex-column align-items-start">
-            <i className="bi bi-journal-code fs-4 educationLogo"></i>
-            <h5 className="m-0 p-0">Full Stack Developer - MERN Stack</h5>
-            <p className="m-0 p-0">Rolling Code School</p>
-            <div className="d-flex">
-              <p className="m-0 p-0 me-2">Status:</p>
-              <p>Completed</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 col-sm-6  d-flex align-items-center justify-content-center text-start">
-          <div className="d-flex flex-column align-items-start">
-            <i className="bi bi-universal-access-circle fs-4 educationLogo"></i>
-            <h5 className="m-0 p-0">Introduction to Web Accessibility</h5>
-            <p className="m-0 p-0">The World Wide Web Consortium (W3C)</p>
-            <div className="d-flex">
-              <p className="m-0 p-0 me-2">Status:</p>
-              <p>In Progress</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 col-sm-6  d-flex text-start mt-4 ">
-          <div className="d-flex flex-column align-items-start">
-            <i className="bi bi-speaker fs-4 educationLogo"></i>
-            <h5 className="m-0 p-0">Music Production</h5>
-            <p className="m-0 p-0">EMC Berklee (BIN)</p>
-            <div className="d-flex">
-              <p className="m-0 p-0 me-2">Status:</p>
-              <p>Completed</p>
-            </div>
+    <div className={darkMode ? "card" : "cardLight"}>
+      <div className="cardbody">
+        <div className="row">
+          <div className="">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
+            nihil dignissimos, aut reprehenderit culpa soluta itaque ad nemo
+            quis fugiat quas officiis iure.
           </div>
         </div>
       </div>
