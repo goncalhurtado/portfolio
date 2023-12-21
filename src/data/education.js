0c3f6e47cbedf9a58513b248ecbe75addc29939a
goncalhurtado <gonzalomilano@gmail.com>
2023-12-21 16:10:08 -0300

    added style to education page

---
+++ src/data/education.js
@@ -0,0 +1,22 @@
+export const education = [{
+        title: "Full Stack Developer - MERN Stack",
+        institution: "Rolling Code School",
+        status: "Completed",
+        date: "",
+        icon: "bi-journal-code fs-4 educationLogo",
+    },
+    {
+        title: "Music Production",
+        institution: "EMC Berklee (BIN)",
+        status: "Completed",
+        date: "",
+        icon: "bi-speaker fs-4 educationLogo",
+    },
+    // {
+    //     title: "Introduction to Web Accessibility",
+    //     institution: "The World Wide Web Consortium (W3C)",
+    //     status: "In Progress",
+    //     date: "",
+    //     icon: "bi-universal-access-circle fs-4 educationLogo",
+    // },
+]
--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { education } from "../data/education";
 
 const Education = ({ darkMode }) => {
   return (
     <div className={darkMode ? "card" : "cardLight"}>
       <div className="cardbody">
-        <div className="row">
+        <div className="d-flex flex-column">
           <div className="">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
-            nihil dignissimos, aut reprehenderit culpa soluta itaque ad nemo
-            quis fugiat quas officiis iure.
+            {education.map((edu, index) => {
+              return (
+                <div key={index}>
+                  <i className={`bi ${edu.icon}`}></i>
+                  <h1 className="">{edu.title}</h1>
+                  <h2 className="">{edu.institution}</h2>
+                  <p className="">
+                    Status: <b>{edu.status}</b>
+                  </p>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
